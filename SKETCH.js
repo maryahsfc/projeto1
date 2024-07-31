18ba2f50977886cc744ca70bffd7dc6d3ff44193
maryahsfc <00001114397726sp@al.educacao.sp.gov.br>
2024-07-31 14:54:18 -0300

    Create SKETCH.js

---
+++ SKETCH.js
@@ -0,0 +1,71 @@
+Skip to Play Sketch
+Cookies
+The p5.js Editor uses cookies. Some are essential to the website functionality and allow you to manage an account and preferences. Others are not essential—they are used for analytics and allow us to learn more about our community. We never sell this data or use it for advertising. You can decide which cookies you would like to allow, and learn more in our Privacy Policy.
+
+
+Allow All
+
+Allow Essential
+Arquivo
+Editar
+Esboço
+Ajuda
+Português
+Olá, maryahgemtg!
+
+
+
+Auto-atualizar
+
+projeto 1
+por maryahgemtg
+
+
+
+sketch.js
+Salvo: 2 meses atrás
+1
+function setup() {
+2
+  createCanvas(1000, 800);
+3
+ background("rgb(231,34,226)");
+4
+}
+5
+​
+6
+function draw() {
+7
+  
+8
+  
+9
+  fill("#DD165A");
+10
+  stroke("lightblue");
+11
+  
+12
+  if (mouseIsPressed);{
+13
+  circle(mouseX,mouseY,20,50);
+14
+  }
+15
+  
+16
+  
+17
+}
+Não há mensagens de Lint
+Linha atuallinha 2
+
+Terminal
+Limpar
+
+47
+
+🌸 p5.js says: [sketch.js, line 13] circle() was expecting no more than 3 arguments, but received 4. (http://p5js.org/reference/#/p5/circle) 
+​
+Prévia
